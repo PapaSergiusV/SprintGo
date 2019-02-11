dc04a3a021545513edf237193cae0df479d20ce3
Ponomarev Sergei <papasergiusv@gmail.com>
2019-02-11 21:11:06 +0300

    Remove Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :confirmable, :validatable
   has_many :roles, dependent: :nullify
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-
+  
   resources :companies do
     resources :roles, only: [:create, :destroy]
     resources :projects do
