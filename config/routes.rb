7c4073da9a3dd0bf69ecc11bfdedf79b9f65a5e1
Ponomarev Sergei <papasergiusv@gmail.com>
2019-02-12 00:05:58 +0300

    Add token auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :items
   resources :companies do
     resources :roles, only: [:create, :destroy]
     resources :projects do
@@ -11,5 +12,8 @@ Rails.application.routes.draw do
 
   get '/companies/:company_id/employees' => 'companies#employees_list'
   get '/roles/companies_list/:user_id' => 'roles#companies_list'
+
+  post 'authenticate' => 'authentication#authenticate'
+  post 'signup' => 'authentication#signup'
 
 end
